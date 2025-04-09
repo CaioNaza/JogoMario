76901cc10e770672ab89ad3d94a30e716e069a34
caio <nazzariobarros@gmail.com>
2025-04-09 16:20:03 -0300

    adding background, ajustes de animação, game over e play again

--- script.js
+++ script.js
@@ -1,4 +1,7 @@
 const mario = document.querySelector('.mario'); 
+const pipe = document.querySelector('.pipe');
+
+
 
 const jump = () => {
     
@@ -11,7 +14,32 @@ const jump = () => {
 
 }
 document.addEventListener('keydown', (jump))
+
 // Toque na tela (mobile)
 document.addEventListener('touchstart', () => {
     jump();
   });
+
+  
+const loop  = setInterval(() => {
+    
+    const pipePosition = pipe.offsetLeft;
+    const marioPosition = window.getComputedStyle(mario).bottom.replace('px', '');
+
+        if(pipePosition <= 120 && pipePosition > 0 && marioPosition < 80) {
+            
+            pipe.style.animation = 'none';
+
+            pipe.style.left = `${pipePosition}px`;
+
+            mario.style.animation = 'none';
+            mario.style.bottom = `${marioPosition}px`;
+
+            mario.src = './images/game-over.png';
+            
+            clearInterval(loop);
+        }
+    
+
+},10);
+document.addEventListener('keydown', jump);
